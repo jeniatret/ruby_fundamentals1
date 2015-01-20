f8e3274c8ad111e9b7032f6d8ac38a7f2c4cc7f3
Jenia Tretiyakova <zhanyat@gmail.com>
2015-01-20 14:56:55 -0500

    I multiplied the numbers and used string interpolation to put it out

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,7 @@ puts 55 * 0.15
 
 # adding a string and an integer. Convert integer and puts the answer
 puts "The tip is: " + 8.25.to_s
+
+#output result in a sentence by using string interpolation
+number = 45628 * 7839
+puts "The value is #{number}"
